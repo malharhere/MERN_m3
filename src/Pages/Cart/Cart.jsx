6a65bcdb0bae711b5768db0909a5cd7b0a389352
malharhere <malharkanthariya03@gmail.com>
2024-11-15 20:01:24 +0530

    4 edit

--- src/Pages/Cart/Cart.jsx
+++ src/Pages/Cart/Cart.jsx
@@ -13,7 +13,7 @@ const Cart = () => {
     <div className='cart'>
       <div className="cart-items">
         <div className="cart-items-title">
-          <p>Items</p>
+          <p>Items33</p>
           <p>Title</p>
           <p>Price</p>
           <p>Quantity</p>
